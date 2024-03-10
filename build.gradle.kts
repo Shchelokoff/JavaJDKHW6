c17c7445f5ab3256dfd7dcf2349c20b0095d190c
ShchelokoffMax <125723518+Shchelokoff@users.noreply.github.com>
2024-03-11 01:37:18 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("io.projectlombok:lombok:1.18.20") // Исправленная зависимость Lombok
-    kapt("io.projectlombok:lombok:1.18.20") // Исправленная зависимость Lombok для Kapt
+    implementation("io.projectlombok:lombok:1.18.20")
+    kapt("io.projectlombok:lombok:1.18.20")
     implementation("org.apache.commons:commons-math3:3.6.1")
 }
 
 application {
-    mainClass.set("org.example.Main") // Замените на полное имя вашего основного класса
+    mainClass.set("org.example.Main")
 }
 
 tasks.named<Test>("test") {
